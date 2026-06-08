cc893f3084951a6bc33ba60ff62ef1f045bc2cc4
burhansa25 <bsacollegeone@gmail.com>
2026-06-09 02:02:55 +0700

    fine and refine the configs for tesitngs

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -10,11 +10,10 @@ const config: Config = {
 
   collectCoverage: true,
 
+  setupFiles: ['<rootDir>/tests/setup-env.ts'],
+
   coverageThreshold: {
     global: {
-      'ts-jest': {
-        isolatedModules: true
-      },
       branches: 50,
       functions: 50,
       lines: 50,
